15796a05fdf220f12b5eef5db9b8d5ad9654378a
code-hopper <ameykhondekar01@gmail.com>
2025-03-18 22:28:03 +0530

    responsive navbar started

--- src/components/includes/header.jsx
+++ src/components/includes/header.jsx
@@ -1,48 +1,50 @@
-import React from 'react'
+"use client";
 
-import { FaCode } from "react-icons/fa"
+import { useState } from 'react';
+import { Menu, X } from 'lucide-react';
+import { FaCode } from "react-icons/fa";
 
-const Header = () => {
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <>
-            <header id='header'>
-                <nav className='flex justify-between items-center bg-stone-950 text-white px-[var(--sidePadding)] py-3'>
-                    <div className='navbar-logo flex gap-1 items-center'>
-                        <div className='navbar-logo-icon bg-[var(--primaryColor)] p-2 rounded-full'>
-                            <FaCode size={"20px"} className='text-[var(--secondaryColor)]' />
-                        </div>
-                        <div className='navbar-logo-text text-2xl'>
-                            <span className='font-semibold'>Code</span>
-                            <span className='font-extrabold text-[var(--primaryColor)]'>Hopper</span>
-                        </div>
+        <header id='header'>
+            <nav className='flex justify-between items-center bg-stone-950 text-white px-[var(--sidePadding)] py-3'>
+                <div className='navbar-logo flex gap-1 items-center'>
+                    <div className='navbar-logo-icon bg-[var(--primaryColor)] p-2 rounded-full'>
+                        <FaCode size={"20px"} className='text-[var(--secondaryColor)]' />
                     </div>
-                    <div className='navbar-menu'>
-                        <ul className='menu-container'>
-                            <li className='menu-container-item'>
-                                <a href="">Home </a>
-                            </li>
-                            <li className='menu-container-item'>
-                                <a href="">Services</a>
-                            </li>
-                            <li className='menu-container-item'>
-                                <a href="">About Me</a>
-                            </li>
-                            <li className='menu-container-item'>
-                                <a href="">Projects</a>
-                            </li>
-                            <li className='menu-container-item'>
-                                <a href="">Blogs</a>
-                            </li>
-                        </ul>
+                    <div className='navbar-logo-text text-2xl'>
+                        <span className='font-semibold'>Code</span>
+                        <span className='font-extrabold text-[var(--primaryColor)]'>Hopper</span>
                     </div>
-                    <div className='navbar-cta'>
-                        <button className='bg-[var(--primaryColor)] text-gray-900 font-semibold px-3 py-1 rounded-2xl'> Let's get Started
-                        </button>
-                    </div>
-                </nav>
-            </header>
-        </>
-    )
-}
+                </div>
 
-export default Header
+                {/* Hamburger Icon for Mobile */}
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className='md:hidden focus:outline-none'
+                >
+                    {isOpen ? <X size={28} /> : <Menu size={28} />}
+                </button>
+
+                <div className={`
+                    navbar-menu md:flex md:flex-row md:items-center
+                    ${isOpen ? 'flex flex-col' : 'hidden'}
+                `}>
+                    <ul className='menu-container'>
+                        <li className='menu-container-item'><a href="">Home</a></li>
+                        <li className='menu-container-item'><a href="">Services</a></li>
+                        <li className='menu-container-item'><a href="">About Me</a></li>
+                        <li className='menu-container-item'><a href="">Projects</a></li>
+                        <li className='menu-container-item'><a href="">Blogs</a></li>
+                    </ul>
+                </div>
+
+                <div className='navbar-cta'>
+                    <button className='bg-[var(--primaryColor)] text-gray-900 font-semibold px-3 py-1 rounded-2xl'>Let's get Started</button>
+                </div>
+            </nav>
+        </header>
+    );
+}
