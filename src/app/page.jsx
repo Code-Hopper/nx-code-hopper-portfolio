12578118835d681df4dec4dc93aae6ddf61c7ef9
code-hopper <ameykhondekar01@gmail.com>
2025-03-18 22:17:06 +0530

    navbar updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const page = () => {
   return (  
     <div>
-        <h1 className='text-center font-extrabold lg:text-gray-900 text-red-500 p-5 bg-yellow-300 text-2xl hover:text-blue-500 transition duration-900 ease-in'>Hello Tailwind</h1>
-        <h2 className='text-center font-extrabold text-red-500 p-5 bg-yellow-300 rounded-2xl'>Hello Tailwind</h2>
-        <span className='block w-full p-10 text-center font-extrabold text-2xl bg-red-500 text-[var(--primaryColor)] hover:text-white transition duration-500 ease-in'>duck</span>
+        
     </div>
   )
 }
--- src/components/includes/header.jsx
+++ src/components/includes/header.jsx
@@ -6,22 +6,39 @@ const Header = () => {
     return (
         <>
             <header id='header'>
-                <nav className='flex bg-stone-950 text-white px-5 py-3'>
-                    <div className='flex items-center gap-2'>
-                        <div className='bg-yellow-300 inline-block p-3 rounded-full'>
-                            <FaCode size={"25px"} className='text-gray-900' />
+                <nav className='flex justify-between items-center bg-stone-950 text-white px-[var(--sidePadding)] py-3'>
+                    <div className='navbar-logo flex gap-1 items-center'>
+                        <div className='navbar-logo-icon bg-[var(--primaryColor)] p-2 rounded-full'>
+                            <FaCode size={"20px"} className='text-[var(--secondaryColor)]' />
                         </div>
-                        <span className='text-2xl'>
-                            Code <span className='font-extrabold'>Hopper</span>
-                        </span>
+                        <div className='navbar-logo-text text-2xl'>
+                            <span className='font-semibold'>Code</span>
+                            <span className='font-extrabold text-[var(--primaryColor)]'>Hopper</span>
+                        </div>
                     </div>
-                    <ul className='ms-auto flex gap-3'>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li id='hello'>
-                            <span>service</span>
-                        </li>
-                    </ul>
+                    <div className='navbar-menu'>
+                        <ul className='menu-container'>
+                            <li className='menu-container-item'>
+                                <a href="">Home </a>
+                            </li>
+                            <li className='menu-container-item'>
+                                <a href="">Services</a>
+                            </li>
+                            <li className='menu-container-item'>
+                                <a href="">About Me</a>
+                            </li>
+                            <li className='menu-container-item'>
+                                <a href="">Projects</a>
+                            </li>
+                            <li className='menu-container-item'>
+                                <a href="">Blogs</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='navbar-cta'>
+                        <button className='bg-[var(--primaryColor)] text-gray-900 font-semibold px-3 py-1 rounded-2xl'> Let's get Started
+                        </button>
+                    </div>
                 </nav>
             </header>
         </>
