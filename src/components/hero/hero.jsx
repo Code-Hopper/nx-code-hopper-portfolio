d0919901819ac4f9f9cf9b602a89559b40e525f2
code-hopper <ameykhondekar01@gmail.com>
2025-03-19 18:45:42 +0530

    heros section

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const heros = () => {
+  return (
+    <>
+      <div className='flex gap-2.5'>
+        <div className='hero-text-content'></div>
+        <div className='hero-main-image-content'></div>
+      </div>
+    </>
+  )
+}
+
+export default heros
