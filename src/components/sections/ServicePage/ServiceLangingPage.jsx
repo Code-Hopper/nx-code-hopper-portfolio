1e4ec96fb077fed8038a80c887f9aee4a50e4b19
code-hopper <ameykhondekar01@gmail.com>
2025-04-04 19:14:48 +0530

    service card in landing page

--- src/components/sections/ServicePage/ServiceLangingPage.jsx
+++ src/components/sections/ServicePage/ServiceLangingPage.jsx
@@ -6,6 +6,78 @@ import LandingPageForm from './LandingPageForm'
 
 const ServiceLangingPage = ({ data }) => {
 
+    let service_landing_page_cards_data = [
+        {
+            title_name: "PORTFOLIO WEBSITE",
+            description: "Website ravida surna eveti semen the conse tusio anivite dianne one nivam acestion vue artin dictum.",
+            number: "01"
+        },
+        {
+            title_name: "E-COMMERCE WEBSITE",
+            description: "Website ravida surna eveti semen the conse tusio anivite dianne one nivam acestion vue artin dictum.",
+            number: "02"
+        },
+        {
+            title_name: "BLOGING WEBSITE",
+            description: "Website ravida surna eveti semen the conse tusio anivite dianne one nivam acestion vue artin dictum.",
+            number: "03"
+        },
+        {
+            title_name: "CUSTOM APPLICATION",
+            description: "Website ravida surna eveti semen the conse tusio anivite dianne one nivam acestion vue artin dictum.",
+            number: "04"
+        },
+        {
+            title_name: "LANDING PAGES",
+            description: "Website ravida surna eveti semen the conse tusio anivite dianne one nivam acestion vue artin dictum.",
+            number: "05"
+        },
+        {
+            title_name: "CMS WEBSITE",
+            description: "Website ravida surna eveti semen the conse tusio anivite dianne one nivam acestion vue artin dictum.",
+            number: "06"
+        }
+    ]
+
+    let service_landing_page_cards_display = (item, index) => {
+        return (
+            <div
+                key={index}
+                className="service-card shadow-xl cursor-pointer snap-start shrink-0 py-8 px-6 bg-white flex flex-col items-start gap-3"
+            >
+                <svg
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2"
+                    stroke="#000000"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="card-icon text-5xl h-12 w-12 stroke-gray-800"
+                >
+                    <rect ry="2" rx="2" height="14" width="20" y="3" x="2"></rect>
+                    <line y2="21" x2="16" y1="21" x1="8"></line>
+                    <line y2="21" x2="12" y1="17" x1="12"></line>
+                </svg>
+
+                <p className="card-title font-bold text-2xl">
+                    {item.title_name}
+                </p>
+                <p className="text-gray-400 text-sm">
+                    {item.description}
+                </p>
+                <p
+                    style={{
+                        "-webkit-text-stroke": '1px gray',
+                        "-webkit-text-fill-color": 'transparent'
+                    }}
+                    className="text-5xl font-bold self-end"
+                >
+                    {item.number}
+                </p>
+            </div>
+        )
+    }
+
     return (
         <>
             <div className='service-landing-page'>
@@ -52,27 +124,26 @@ const ServiceLangingPage = ({ data }) => {
                                 {/* <!-- From Uiverse.io by Creatlydev --> */}
                                 <button className="button">
                                     <p className="button__text">
-                                        <span style={{"--index": 0}}>C</span>
-                                        <span style={{"--index": 1}}>h</span>
-                                        <span style={{"--index": 2}}>e</span>
-                                        <span style={{"--index": 3}}>c</span>
-                                        <span style={{"--index": 4}}>k</span>
-                                        <span style={{"--index": 5}}> </span>
-                                        <span style={{"--index": 6}}>T</span>
-                                        <span style={{"--index": 7}}>h</span>
-                                        <span style={{"--index": 8}}>i</span>
-                                        <span style={{"--index": 9}}>s</span>
-                                        <span style={{"--index": 10}}> </span>
-                                        <span style={{"--index": 11}}>O</span>
-                                        <span style={{"--index": 12}}>u</span>
-                                        <span style={{"--index": 13}}>t</span>
+                                        <span style={{ "--index": 0 }}>C</span>
+                                        <span style={{ "--index": 1 }}>h</span>
+                                        <span style={{ "--index": 2 }}>e</span>
+                                        <span style={{ "--index": 3 }}>c</span>
+                                        <span style={{ "--index": 4 }}>k</span>
+                                        <span style={{ "--index": 5 }}> </span>
+                                        <span style={{ "--index": 6 }}>T</span>
+                                        <span style={{ "--index": 7 }}>h</span>
+                                        <span style={{ "--index": 8 }}>i</span>
+                                        <span style={{ "--index": 9 }}>s</span>
+                                        <span style={{ "--index": 10 }}> </span>
+                                        <span style={{ "--index": 11 }}>O</span>
+                                        <span style={{ "--index": 12 }}>u</span>
+                                        <span style={{ "--index": 13 }}>t</span>
                                     </p>
 
                                     <div className="button__circle">
                                         <svg
                                             viewBox="0 0 14 15"
                                             fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
                                             className="button__icon"
                                             width="14"
                                         >
@@ -110,9 +181,27 @@ const ServiceLangingPage = ({ data }) => {
                     </div>
                 </div>
 
+                {/* service page cards */}
+
+                <div className='service-landing-page-reading'>
+
+                    <h1 className='py-5 text-center font-bold border-b-2'>I Can Help You With !</h1>
+
+                    <div className="content-container !py-10">
+
+                        <div className='service-landing-page-cards'>
+                                {
+                                    service_landing_page_cards_data.map(service_landing_page_cards_display)
+                                }
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
         </>
     )
 }
 
 export default ServiceLangingPage
+
